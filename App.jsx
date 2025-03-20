773dc7641336cb2ab822d69a36e052e0c5be97ef
Aima Khan <167219913+DeveloperAima@users.noreply.github.com>
2025-03-20 15:28:30 +0000

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomeNavbar from "./HomePageComponents/HomeNavbar";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+
+
+function App() {
+  return (
+    <Router>
+      <HomeNavbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
